dd442431c1efa9d4befd680a9e63762985f72190
Gao <1520045270@qq.com>
2021-04-04 11:29:07 +0800

    单页面应用+Vue-Router+Vuex

---
+++ Component/demo_01/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <input v-model="inputValue" />
+    <button @click="handleAddItem" class="button">提交</button>
+  </div>
+  <ul>
+    <ListItem v-for="(item,index) in list" :key="index" :msg="item" />
+  </ul>
+</template>
+
+<script>
+//单文件组件
+import { reactive, ref } from "vue";
+import ListItem from "./components/ListItem";
+export default {
+  name: "App",
+  components: {
+    ListItem
+  },
+  setup() {
+    const inputValue = ref("");
+    const list = reactive([]);
+    const handleAddItem = () => {
+      list.push(inputValue.value);
+      inputValue.value = "";
+    };
+    return { inputValue, list, handleAddItem };
+  }
+};
+</script>
+
+<style>
+.button {
+  margin-left: 20px;
+  color: red;
+}
+</style>
---
+++ Component/demo_02/src/views/Login.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="home">
+    loginpage
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginPage"
+};
+</script>
